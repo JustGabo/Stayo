44b7f174e89e62e59a2d376926fac85c10e57e64
JustGabo <ovallegabriel123@gmail.com>
2024-03-23 21:23:26 -0700

    feat: trying to resolve a production dynamic problem

--- app/api/favorites/[listingId]/route.ts
+++ app/api/favorites/[listingId]/route.ts
@@ -1,68 +1,34 @@
-import {NextResponse} from 'next/server'
+import { NextResponse } from "next/server";
 
-import getCurrentUser from '@/app/actions/getCurrentUser'
-import prisma from '@/app/libs/prismadb'
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import prisma from "@/app/libs/prismadb";
 
-interface IParams{
-    listingId?: string;
+interface IParams {
+  listingId?: string;
 }
 
-export async function POST(request: Request, {params}: {params: IParams}){
+export async function DELETE(
+  request: Request, 
+  { params }: { params: IParams }
+) {
+  const currentUser = await getCurrentUser();
 
-    const currentUser = await getCurrentUser()
+  if (!currentUser) {
+    return NextResponse.error();
+  }
 
-    if(!currentUser){
-        return NextResponse.error()
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== 'string') {
+    throw new Error('Invalid ID');
+  }
+
+  const listing = await prisma.listing.deleteMany({
+    where: {
+      id: listingId,
+      userId: currentUser.id
     }
+  });
 
-    const {listingId} = params;
-
-    if(!listingId || typeof listingId !== 'string'){
-        throw new Error('Invalid ID')
-    }
-
-    let favoriteIds = [...(currentUser.favoriteIds || [])]
-
-    favoriteIds.push(listingId)
-
-    const user = await prisma.user.update({
-        where:{
-            id: currentUser.id,
-        },
-        data:{
-            favoriteIds
-        }
-    })
-
-    return NextResponse.json(user)
-}
-
-
-export async function DELETE(request: Request, {params}: {params: IParams}){
-    const currentUser = await getCurrentUser()
-
-    if(!currentUser){
-        return NextResponse.error()
-    }
-
-    const {listingId} = params;
-
-    if(!listingId || typeof listingId !== 'string'){
-        throw new Error('Invalid ID')
-    }
-
-    let favoriteIds = [...(currentUser.favoriteIds || [])]
-
-    favoriteIds = favoriteIds.filter((id)=> id !== listingId)
-
-    const user = await prisma.user.update({
-        where:{
-            id: currentUser.id
-        },
-        data:{
-            favoriteIds
-        }
-    })
-
-    return NextResponse.json(user)
+  return NextResponse.json(listing);
 }
